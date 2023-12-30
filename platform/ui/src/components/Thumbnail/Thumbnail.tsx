eafc745518e54a8acbd54ad4a188af5c9b174871
Shivkaran Ravidas <s.ravidas01@gmail.com>
2023-12-30 22:35:42 +0530

    Thumbnail updated with min h and w size
    Thumbnail updated with min h and w size

--- platform/ui/src/components/Thumbnail/Thumbnail.tsx
+++ platform/ui/src/components/Thumbnail/Thumbnail.tsx
@@ -51,7 +51,7 @@ const Thumbnail = ({
       <div ref={drag}>
         <div
           className={classnames(
-            'min-h-20 flex flex-1 items-center justify-center overflow-hidden rounded-md bg-black text-base text-white',
+            'flex h-14 w-14 flex-1 items-center justify-center overflow-hidden rounded-md bg-black text-base text-white',
             isActive
               ? 'border-primary-light border-2'
               : 'border-secondary-light border hover:border-blue-300'
@@ -64,7 +64,7 @@ const Thumbnail = ({
             <img
               src={imageSrc}
               alt={imageAltText}
-              className="min-h-20 object-fill"
+              className="h-14 w-14 object-fill"
               crossOrigin="anonymous"
             />
           ) : (
